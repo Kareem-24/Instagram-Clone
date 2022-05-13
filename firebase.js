7289da7a89956e7af49f8cc3627b0ba5dc3f94ba
kareem <abdelkareem58@gmail.com>
2022-05-13 16:43:49 +0200

    adjusted nextAuth_URL

--- firebase.js
+++ firebase.js
@@ -1,8 +1,10 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
